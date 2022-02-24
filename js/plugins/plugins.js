3782eb3941473fc51678a18289dddd245dd3b9f3
Fabio Pegorari <fabio.pegorari@gmail.com>
2022-02-24 20:31:13 +0100

    changes

--- js/plugins/plugins.js
+++ js/plugins/plugins.js
@@ -16,7 +16,7 @@ var swiper_hero = new Swiper('.swiper.hero', {
 
 var swiper_feedback = new Swiper('.swiper.client-feedback-left', {
   speed: 2000,
-   loop: true, 
+   loop: true,
    effect: 'fade',
    fadeEffect: {
     crossFade: true
@@ -24,24 +24,24 @@ var swiper_feedback = new Swiper('.swiper.client-feedback-left', {
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
-    },  
+    },
   autoplay: {
     delay: 2500,
-      disableOnInteraction: false, 
+      disableOnInteraction: false,
   },
 
 });
 
 var swiper_videos = new Swiper('.swiper.videos', {
   speed: 500,
-   loop: true, 
+   loop: true,
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
-    },  
+    },
   autoplay: {
     delay: 2500,
-      disableOnInteraction: false, 
+      disableOnInteraction: false,
   },
 
 });
@@ -60,3 +60,44 @@ function indicateScrollBar() {
   const scrolled = (distanceFromPageTop / height) * 100;
   document.querySelector(".scroll-bar").style.width = `${scrolled}%`;
 }
+(function ($) {
+	"use strict";
+// Timeline Scroll Section
+	// --------------------------------------------------------------
+	var items = $(".reacttimeline li, .journey-list li"),
+	timelineHeight = $(".journey-list").height(),
+	greyLine = $('.default-line'),
+	lineToDraw = $('.draw-line');
+  greyLine.height(timelineHeight);
+
+	if(lineToDraw.length) {
+	  	$(window).on('scroll', function () {
+
+	    var redLineHeight = lineToDraw.height(),
+	    greyLineHeight = greyLine.height(),
+	    windowDistance = $(window).scrollTop(),
+	    windowHeight = $(window).height() / 2,
+	    timelineDistance = $(".reacttimeline").offset().top;
+
+		    if(windowDistance >= timelineDistance - windowHeight) {
+		      	var line = windowDistance - timelineDistance + windowHeight;
+		      	if(line <= greyLineHeight) {
+			        lineToDraw.css({
+			          'height' : line + 20 + 'px'
+			        });
+		      	}
+		    }
+
+		    var bottom = lineToDraw.offset().top + lineToDraw.outerHeight(true);
+		    items.each(function(index){
+		      var circlePosition = $(this).offset();
+		      	if(bottom > circlePosition.top) {
+		        	$(this).addClass('in-view');
+		      	} else {
+		        	$(this).removeClass('in-view');
+		      	}
+		    });
+	  	});
+	}
+
+})(jQuery);
