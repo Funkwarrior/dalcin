c431660cf415375b20d0c46cda17605bb7e2c2db
Fabio Pegorari <fabio.pegorari@gmail.com>
2022-05-17 14:39:13 +0200

    changes

--- js/plugins/plugins.js
+++ js/plugins/plugins.js
@@ -16,35 +16,51 @@ var swiper_hero = new Swiper('.swiper.hero', {
 
 var swiper_feedback = new Swiper('.swiper.client-feedback-left', {
   speed: 2000,
-   loop: true,
-   effect: 'fade',
-   fadeEffect: {
+  loop: true,
+  effect: 'fade',
+  fadeEffect: {
     crossFade: true
   },
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
-    },
+  },
   autoplay: {
     delay: 2500,
-      disableOnInteraction: false,
+    disableOnInteraction: false,
   },
 
 });
 
 var swiper_videos = new Swiper('.swiper.videos', {
   speed: 500,
-   loop: true,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
-    },
+  },
   autoplay: {
     delay: 2500,
-      disableOnInteraction: false,
+    disableOnInteraction: false,
   },
 
 });
+
+var swiper_test = new Swiper('.swiper.form-home', {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    prevEl: '.button-prev',
+    nextEl: '.button-next'
+  },
+});
+
 
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#navbarNavDropdown"
@@ -66,43 +82,43 @@ function indicateScrollBar() {
 
 
 (function ($) {
-	"use strict";
-// Timeline Scroll Section
-	// --------------------------------------------------------------
-	var items = $(".reacttimeline li, .journey-list li"),
-	timelineHeight = $(".journey-list").height(),
-	greyLine = $('.default-line'),
-	lineToDraw = $('.draw-line');
+  "use strict";
+  // Timeline Scroll Section
+  // --------------------------------------------------------------
+  var items = $(".reacttimeline li, .journey-list li"),
+    timelineHeight = $(".journey-list").height(),
+    greyLine = $('.default-line'),
+    lineToDraw = $('.draw-line');
   greyLine.height(timelineHeight);
 
-	if(lineToDraw.length) {
-	  	$(window).on('scroll', function () {
+  if (lineToDraw.length) {
+    $(window).on('scroll', function () {
 
-	    var redLineHeight = lineToDraw.height(),
-	    greyLineHeight = greyLine.height(),
-	    windowDistance = $(window).scrollTop(),
-	    windowHeight = $(window).height() / 2,
-	    timelineDistance = $(".reacttimeline").offset().top;
+      var redLineHeight = lineToDraw.height(),
+        greyLineHeight = greyLine.height(),
+        windowDistance = $(window).scrollTop(),
+        windowHeight = $(window).height() / 2,
+        timelineDistance = $(".reacttimeline").offset().top;
 
-		    if(windowDistance >= timelineDistance - windowHeight) {
-		      	var line = windowDistance - timelineDistance + windowHeight;
-		      	if(line <= greyLineHeight) {
-			        lineToDraw.css({
-			          'height' : line + 20 + 'px'
-			        });
-		      	}
-		    }
+      if (windowDistance >= timelineDistance - windowHeight) {
+        var line = windowDistance - timelineDistance + windowHeight;
+        if (line <= greyLineHeight) {
+          lineToDraw.css({
+            'height': line + 20 + 'px'
+          });
+        }
+      }
 
-		    var bottom = lineToDraw.offset().top + lineToDraw.outerHeight(true);
-		    items.each(function(index){
-		      var circlePosition = $(this).offset();
-		      	if(bottom > circlePosition.top) {
-		        	$(this).addClass('in-view');
-		      	} else {
-		        	$(this).removeClass('in-view');
-		      	}
-		    });
-	  	});
-	}
+      var bottom = lineToDraw.offset().top + lineToDraw.outerHeight(true);
+      items.each(function (index) {
+        var circlePosition = $(this).offset();
+        if (bottom > circlePosition.top) {
+          $(this).addClass('in-view');
+        } else {
+          $(this).removeClass('in-view');
+        }
+      });
+    });
+  }
 
 })(jQuery);
